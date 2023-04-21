6cb50e3fa3e7652848d5d0c7c4ea36430a6f604c
squark11 <kacper.mackowiak@microsoft.wsei.edu.pl>
2023-04-21 21:35:49 +0200

    done in one component

--- generator-css/src/app/app.component.ts
+++ generator-css/src/app/app.component.ts
@@ -9,9 +9,24 @@ export class AppComponent {
   title = 'generator-css';
 
   protected css: CSS = {
-    color: undefined,
-    border: undefined,
-    'box-shadow': undefined,
-    background: undefined
+    color: "red",
+    border: "2px solid blue",
+    boxShadow: "0px 0px 10.5px rgba(0, 0, 0, 0.3)",
+    background: "#ddd"
+  }
+
+  
+
+  onColorInputChange(value:string){
+    this.css.color = value;
+  }
+  onBorderInputChange(value:string){
+    this.css.border = value;
+  }
+  onboxShadowInputChange(value:string){
+    this.css.boxShadow = value;
+  }
+  onBackgroundInputChange(value:string){
+    this.css.background = value;
   }
 }
